f3dbc81bc781db4809e71c8165a2ca1a7b7613e9
MSR <msujithr@gmail.com>
2023-09-03 17:04:24 +0530

    Initial commit

---
+++ test/knowledge_cloud_server.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KnowledgeCloudServer from '../lib/knowledge_cloud_server-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/knowledge_cloud_server-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KnowledgeCloudServer.KnowledgeCloudServerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
